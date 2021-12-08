fe4f7ac0631db1705e85fb7a7bf91d5e550e2287
Victor Leiderö-Palmlöv <victorleideropalmlov@gmail.com>
2021-12-08 11:08:24 +0100

    Förbättrningar och point system

--- 2D_TopDown/Program.cs
+++ 2D_TopDown/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Raylib_cs;
+using System.Collections.Generic;
 
 Raylib.InitWindow(800, 600, "Topdown Game");
 Raylib.SetTargetFPS(60);
@@ -7,92 +8,87 @@ Raylib.SetTargetFPS(60);
 Texture2D playerImage = Raylib.LoadTexture("amogus.png");
 Texture2D ventImage = Raylib.LoadTexture("vent.png");
 
-//float x = 4.5f;
-
-//float y = 10;
-
-//int z = (int) 4.6f;
-
 Rectangle playerRect = new Rectangle(10, 10, playerImage.width, playerImage.height);
-Rectangle ventRect = new Rectangle(400, 375, ventImage.width, ventImage.height);
+Rectangle ventRect = new Rectangle(625, 375, ventImage.width, ventImage.height);
 Rectangle overlap = Raylib.GetCollisionRec(playerRect, ventRect);
 
+Rectangle pointRec = new Rectangle(80, 375, 18, 18);
+Rectangle overlapPoint = Raylib.GetCollisionRec(playerRect, pointRec);
 
 Color pcolor = Color.BLUE;
 Color ecolor = Color.RED;
 Color ocolor = Color.PURPLE;
+Color pointcolor = Color.LIME;
 
-string room = "1";
+string room = "Lobby";
+
+int pointsint = 0;
+
+bool pointCollected = false;
 
 while (!Raylib.WindowShouldClose())
 {
 
     int speed = 3;
 
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT_SHIFT))
-    {
-        speed = 5;
-    }
-
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
-    {
-        playerRect.x += speed;
-    }
-
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
-    {
-        playerRect.x -= speed;
-    }
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
-    {
-        playerRect.y -= speed;
-    }
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
-    {
-        playerRect.y += speed;
-    }
-
-
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT_SHIFT)) speed = 5;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) playerRect.x += speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) playerRect.x -= speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) playerRect.y -= speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) playerRect.y += speed;
+    
+    if (playerRect.x < 0) playerRect.x = 0;
+    if (playerRect.y < 0) playerRect.y = 0;
+    if (playerRect.x > 800 - playerRect.width) playerRect.x = 800 - playerRect.width;
+    if (playerRect.y > 600 - playerRect.height) playerRect.y = 600 - playerRect.height;
+    
     Raylib.BeginDrawing();
 
-    if (room == "1")
+    Raylib.DrawText(room, 350, 10, 20, Color.WHITE);
+    Raylib.DrawText(pointsint.ToString(), 20, 10, 40, Color.WHITE);
+
+
+    if (room == "Lobby")
     {
-
-
         bool areOverlapping = Raylib.CheckCollisionRecs(playerRect, ventRect);
+        bool pointOverlapping = Raylib.CheckCollisionRecs(playerRect, pointRec);
 
         Raylib.ClearBackground(Color.BLACK);
         Raylib.DrawRectangleRec(playerRect, pcolor);
         Raylib.DrawRectangleRec(ventRect, ecolor);
+        
+        if (pointCollected == false)
+        {
+            Raylib.DrawRectangleRec(pointRec, pointcolor);
+        }
 
         Raylib.DrawTexture(playerImage, (int)playerRect.x, (int)playerRect.y, Color.BLUE);
         Raylib.DrawTexture(ventImage, (int)ventRect.x, (int)ventRect.y, Color.RED);
 
         if (areOverlapping == true)
         {
-            room = "2";
+            room = "Hallway";
+        }
+        else if (pointOverlapping == true && pointCollected == false)
+        {
+            pointsint++;
+            pointCollected = true;
         }
 
     }
 
-    if (room == "2")
+    if (room == "Hallway")
     {
-        Raylib.ClearBackground(Color.GRAY);
+        Raylib.ClearBackground(Color.DARKGRAY);
         Raylib.DrawRectangleRec(playerRect, pcolor);
 
         Raylib.DrawTexture(playerImage, (int)playerRect.x, (int)playerRect.y, Color.BLUE);
 
     }
-
-
-
-
-
-
-
-
-
-
+    
+    
+    
     Raylib.EndDrawing();
+
 
 }
